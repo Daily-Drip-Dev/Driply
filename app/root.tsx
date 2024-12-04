1f93a39f1d10b0b37906293b1f8446d2fb486932
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2024-12-04 14:52:11 +0900

    chore: stitches 세팅 (#5)
    * chore: stitches theme color 설정
    
    * feat: body 태그 스타일 초기화 및 Noto Sans KR 폰트 설정
    
    * feat: SSR stitches 적용

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,7 @@ import { ApolloProvider } from "@apollo/client/index.js";
 import apolloClient from "src/apollo/client";
 
 import styles from "./index.css?url";
+import { getCssText } from "src/stitches";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -15,7 +16,7 @@ export const links: LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap",
   },
   { rel: "stylesheet", href: styles },
 ];
@@ -28,6 +29,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       </head>
       <body>
         {children}
---
+++ src/stitches.ts
@@ -0,0 +1,23 @@
+import { createStitches } from "@stitches/react";
+
+export const { styled, css, getCssText } = createStitches({
+  theme: {
+    colors: {
+      primary: "#DE4B18",
+      black: "#12100E",
+      white: "#F7F6F4",
+
+      warmGray50: "#F7F6F4",
+      warmGray100: "#EFEDEA",
+      warmGray200: "#D9D5CE",
+      warmGray300: "#C4BDB2",
+      warmGray400: "#AFA597",
+      warmGray500: "#998D7B",
+      warmGray600: "#766F69",
+      warmGray700: "#655B4E",
+      warmGray800: "#494238",
+      warmGray900: "#2E2923",
+      warmGray950: "#12100E",
+    },
+  },
+});
