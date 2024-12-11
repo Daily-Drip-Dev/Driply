a3c4c3e9ed68ac8092c7386ee28f63a28287faf2
Jeongwoo Park <wahoo9040@naver.com>
2024-12-11 10:24:46 +0900

    chore: GetCoffeeList Mocking Handler 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,17 +4,15 @@ const handlers = [
   graphql.query("GetCoffeeList", () => {
     const coffeeList = [
       {
-        id: "1",
         title: "아이스 아메리카노",
         score: 7,
         img_url: "",
         created_at: "2024-12-01T12:34:56Z",
       },
       {
-        id: "2",
         title: "따듯한 라떼",
         score: 9,
-        img_url: "",
+        img_url: "https://via.placeholder.com/60",
         created_at: "2024-12-02T15:30:00Z",
       },
     ];
@@ -22,9 +20,11 @@ const handlers = [
     return HttpResponse.json({
       data: {
         coffeeCollection: {
-          edges: coffeeList.map((coffee) => ({
-            node: coffee,
-          })),
+          edges: Array.from({ length: 10 }, () => [...coffeeList])
+            .flatMap((v) => v)
+            .map((coffee, idx) => ({
+              node: { ...coffee, id: idx },
+            })),
         },
       },
     });
