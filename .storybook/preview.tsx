4f61f2ac6a3e93b92da01ef8007021c3a4b67809
Jeongwoo Park <wahoo9040@naver.com>
2024-12-11 10:22:16 +0900

    chore: Storybook msw 세팅

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,15 @@
 import type { Preview } from "@storybook/react";
 import React from "react";
+
+import { ApolloProvider } from "@apollo/client";
+import apolloClient from "../src/apollo/client";
+import handlers from "../src/mocks/handlers.ts";
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
 import { defaultTheme } from "../src/vanilla-extract/theme.css";
 import "../src/vanilla-extract/globalStyles.css";
+
+initialize();
 
 const preview: Preview = {
   parameters: {
@@ -11,13 +19,18 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    msw: {
+      handlers,
+    },
   },
-
   decorators: [
+    mswDecorator,
     (Story) => (
-      <div className={defaultTheme}>
-        <Story />
-      </div>
+      <ApolloProvider client={apolloClient}>
+        <div className={defaultTheme}>
+          <Story />
+        </div>
+      </ApolloProvider>
     ),
   ],
 };
