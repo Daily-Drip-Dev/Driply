69bf5e47308904ad129ac7b3e537a97d6a2c05d2
Jeongwoo Park <wahoo9040@naver.com>
2024-12-11 13:39:47 +0900

    feat: LogsLayout 주석 작성

--- app/layout/LogsLayout/index.tsx
+++ app/layout/LogsLayout/index.tsx
@@ -15,6 +15,8 @@ export default function LogsLayout() {
         </LogsHeader>
       </header>
       <main className={styles.logsMain}>
+        {/* TODO: Error Fallback 구현 */}
+        {/* 러기: [24.12.11] Coffee List가 없을 때 Error를 던지고, Fallback 구현을 여기서 하면 어떨까요? */}
         <LogsMain />
       </main>
     </div>
