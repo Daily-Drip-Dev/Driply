516b982a185911862affa78b8da3615f70179833
Kim Da Eun <midekuna@gmail.com>
2025-01-06 00:33:12 +0900

    feat: 카카오 로그인
    chore: 주석 제거
    
    feat: AuthCallback 라우트 추가로 PKCE 공격 방지
    
    refactor: login 라우트 제거
    
    refactor: supabaseServer 인자 변경 대응
    
    refactor: 사용하지 않는 loader data 제거

---
+++ app/routes/authCallback.tsx
@@ -0,0 +1,31 @@
+import { LoaderFunctionArgs, redirect } from '@remix-run/node';
+import supabaseServer from 'src/supabase/server';
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get('code');
+  const next = requestUrl.searchParams.get('next') || '/logs';
+  const headers = new Headers();
+
+  if (code) {
+    const supabase = supabaseServer({ request, headers });
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+    if (error) {
+      const searchParams = new URLSearchParams({
+        errorMessage: error.message,
+      });
+      return redirect(`/${searchParams.toString()}`);
+    }
+    return redirect(next, { headers });
+  }
+
+  const searchParams = new URLSearchParams({
+    errorMessage: '인증 과정에 문제가 발생했습니다.',
+  });
+  return redirect(`/${searchParams.toString()}`);
+}
+
+export default function AuthCallback() {
+  return <div>AuthCallback</div>;
+}
