6e1d3c848108158275aba6259ad07f545f66378f
Jeongwoo Park <wahoo9040@naver.com>
2025-01-29 11:33:32 +0900

    feat: Input 공통 컴포넌트 구현

---
+++ app/component/Input/styles.css.ts
@@ -0,0 +1,32 @@
+import { style } from '@vanilla-extract/css';
+import { colors } from 'src/vanilla-extract/theme.css';
+
+const container = style({
+  position: 'relative',
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+const input = style({
+  width: '100%',
+  padding: '12px',
+  borderRadius: '2px',
+  backgroundColor: colors.warmGray[100],
+  color: colors.warmGray[800],
+  '::placeholder': {
+    color: colors.warmGray[600],
+  },
+});
+
+const counter = style({
+  alignSelf: 'flex-end',
+  color: colors.warmGray[800],
+});
+const styles = {
+  container,
+  input,
+  counter,
+};
+
+export default styles;
