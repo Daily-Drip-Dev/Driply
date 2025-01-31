11d5022ba460f24c9d5c3ccdc7f534ab07928c2b
Kim Da Eun <midekuna@gmail.com>
2025-01-31 18:02:12 +0900

    feat: 커피 점수 슬라이더 구현

--- src/vanilla-extract/theme.css.ts
+++ src/vanilla-extract/theme.css.ts
@@ -1,10 +1,11 @@
-import { createTheme, createThemeContract } from "@vanilla-extract/css";
+import { createTheme, createThemeContract } from '@vanilla-extract/css';
 
 export const colors = createThemeContract({
   base: {
     primary: null,
     black: null,
     white: null,
+    orange: null,
   },
   warmGray: {
     100: null,
@@ -32,30 +33,31 @@ export const colors = createThemeContract({
 
 export const defaultTheme = createTheme(colors, {
   base: {
-    primary: "#DE4B18",
-    black: "#12100E",
-    white: "#F7F6F4",
+    primary: '#DE4B18',
+    black: '#12100E',
+    white: '#F7F6F4',
+    orange: '#DE4B18',
   },
   warmGray: {
-    100: "#EFEDEA",
-    200: "#D9D5CE",
-    300: "#C4BDB2",
-    400: "#AFA597",
-    500: "#998D7B",
-    600: "#766F69",
-    700: "#655B4E",
-    800: "#494238",
-    900: "#2E2923",
+    100: '#EFEDEA',
+    200: '#D9D5CE',
+    300: '#C4BDB2',
+    400: '#AFA597',
+    500: '#998D7B',
+    600: '#766F69',
+    700: '#655B4E',
+    800: '#494238',
+    900: '#2E2923',
   },
   blueScale: {
-    100: "#D9E4ED",
-    200: "#B3C9DB",
-    300: "#8CADC8",
-    400: "#6692B6",
-    500: "#4077A4",
-    600: "#35658D",
-    700: "#2A5376",
-    800: "#204260",
-    900: "#153049",
+    100: '#D9E4ED',
+    200: '#B3C9DB',
+    300: '#8CADC8',
+    400: '#6692B6',
+    500: '#4077A4',
+    600: '#35658D',
+    700: '#2A5376',
+    800: '#204260',
+    900: '#153049',
   },
 });
