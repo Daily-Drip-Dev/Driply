7a408b8530b4879557561ee64d31cc36e25e4824
Jeongwoo Park <121204715+lurgi@users.noreply.github.com>
2025-02-06 19:15:18 +0900

    feat: Cafe검색 기능 구현 (Kakao Maps) (#37)
    * feat: RemovableImage 컴포넌트 구현
    
    * feat: ImageList 기본 구현
    
    * feat: Input 공통 컴포넌트 구현
    
    * feat: TextArea 공통 컴포넌트 구현
    
    * feat: 스크롤바 디자인 수정
    
    * feat: Loader 컴포넌트 구현
    
    * feat: ScrollObserver 구현
    
    IntersectionObserver를 이용하여 구현
    
    * chore: Kakao Maps 타입 의존성 설치
    
    * feat: useKakaoMapsScript 훅 구현
    
    * feat: CafeSearch 컴포넌트 구현
    
    * chore: useKakaoMapsScript 훅 디렉토리 위치 변경
    
    * refactor: useKakaoCafeSearch 훅 구현 및 리팩터링
    
    * feat: CafeSearch컴포넌트 onSearch 구현
    
    * chore: kakao 관련 메서드 주석 추가

--- src/vanilla-extract/globalStyles.css.ts
+++ src/vanilla-extract/globalStyles.css.ts
@@ -1,46 +1,65 @@
-import { globalStyle } from "@vanilla-extract/css";
+import { globalStyle } from '@vanilla-extract/css';
 
-globalStyle("body", {
-  fontFamily: "Noto Sans KR, sans-serif",
-  color: "#12100E",
+globalStyle('body', {
+  fontFamily: 'Noto Sans KR, sans-serif',
+  color: '#12100E',
 });
 
-globalStyle("svg", {
-  verticalAlign: "middle",
+globalStyle('svg', {
+  verticalAlign: 'middle',
 });
 
-globalStyle("h1", {
-  fontSize: "32px",
-  lineHeight: "140%",
+globalStyle('h1', {
+  fontSize: '32px',
+  lineHeight: '140%',
   fontWeight: 900,
 });
 
-globalStyle("h2", {
-  fontSize: "26px",
-  lineHeight: "130%",
+globalStyle('h2', {
+  fontSize: '26px',
+  lineHeight: '130%',
   fontWeight: 700,
 });
 
-globalStyle("h3", {
-  fontSize: "22px",
-  lineHeight: "120%",
+globalStyle('h3', {
+  fontSize: '22px',
+  lineHeight: '120%',
   fontWeight: 500,
 });
 
-globalStyle("h4", {
-  fontSize: "20px",
-  lineHeight: "130%",
+globalStyle('h4', {
+  fontSize: '20px',
+  lineHeight: '130%',
   fontWeight: 500,
 });
 
-globalStyle("h5", {
-  fontSize: "18px",
-  lineHeight: "130%",
+globalStyle('h5', {
+  fontSize: '18px',
+  lineHeight: '130%',
   fontWeight: 500,
 });
 
-globalStyle("p", {
-  fontSize: "16px",
-  lineHeight: "130%",
+globalStyle('p', {
+  fontSize: '16px',
+  lineHeight: '130%',
   fontWeight: 400,
+});
+
+globalStyle('::-webkit-scrollbar', {
+  width: '10px',
+  height: '10px',
+});
+
+globalStyle('::-webkit-scrollbar-track', {
+  background: '#f1f1f1',
+  borderRadius: '5px',
+});
+
+globalStyle('::-webkit-scrollbar-thumb', {
+  background: '#C4BDB2',
+  borderRadius: '5px',
+});
+
+globalStyle('::-webkit-scrollbar-thumb:hover', {
+  background: '#998D7B',
 });
