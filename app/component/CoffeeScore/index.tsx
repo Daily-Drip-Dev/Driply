9e503dd1ff028d3d800d4be9afbcb1cf209c72b1
Kim Da Eun <midekuna@gmail.com>
2025-02-17 21:18:49 +0900

    feat: score 전역변수 생성

--- app/component/CoffeeScore/index.tsx
+++ app/component/CoffeeScore/index.tsx
@@ -4,12 +4,16 @@ import { styles } from './style.css';
 
 type ScoreTitle = keyof typeof COFFEE_SCORE_TITLE;
 
+function isScoreTitle(key: string): key is ScoreTitle {
+  return key in COFFEE_SCORE_TITLE;
+}
+
 export default function CoffeeScore() {
   return (
     <div className={styles.container}>
       {Object.keys(COFFEE_SCORE_TITLE).map((title) => {
-        if (title in COFFEE_SCORE_TITLE) {
-          return <CoffeeScoreSlider key={title} title={title as ScoreTitle} />;
+        if (isScoreTitle(title)) {
+          return <CoffeeScoreSlider key={title} title={title} />;
         }
       })}
     </div>
--- src/store/useCoffeeLogForm.ts
+++ src/store/useCoffeeLogForm.ts
@@ -1,8 +1,7 @@
-import { arrayMove } from '@dnd-kit/sortable';
+import { COFFEE_SCORE_TITLE } from 'src/constants';
 import { create } from 'zustand';
 
 interface ImageState {
-  id: string;
   file: File;
   previewUrl: string;
 }
@@ -11,45 +10,55 @@ interface CoffeeLogFormState {
   images: ImageState[];
   pushImages: (files: File[]) => void;
   deleteImage: (index: number) => void;
-  switchImageOrder: (currentId: string, targetId: string) => void;
 
   // TODO: 카카오 위치에 대한 정보 저장 필요
   title: string;
   description: string;
   setTitle: (title: string) => void;
   setDescription: (title: string) => void;
+
+  scores: {
+    flavor: 0;
+    sweetness: 0;
+    acidity: 0;
+    balance: 0;
+    overall: 0;
+  };
+  setScore: (key: keyof typeof COFFEE_SCORE_TITLE, value: number) => void;
 }
 
 export const useCoffeeLogForm = create<CoffeeLogFormState>((set, get) => ({
   images: [],
   pushImages: (files) => {
-    const transformedFiles = files.map((file) => {
-      const url = URL.createObjectURL(file);
-      return {
-        id: url,
-        file,
-        previewUrl: url,
-      };
-    });
+    const transformedFiles = files.map((file) => ({
+      file,
+      previewUrl: URL.createObjectURL(file),
+    }));
     set({ images: [...get().images, ...transformedFiles] });
   },
   deleteImage: (index) => {
     const curImages = get().images;
-    URL.revokeObjectURL(curImages[index].previewUrl);
     set({ images: [...curImages.slice(0, index), ...curImages.slice(index + 1)] });
-  },
-  switchImageOrder: (currentId, targetId) => {
-    const prevImages = get().images;
-    const currentIndex = prevImages.findIndex(({ id }) => id === currentId);
-    const targetIndex = prevImages.findIndex(({ id }) => id === targetId);
-
-    if (currentIndex === targetIndex) return;
-    const newImages = arrayMove(prevImages, currentIndex, targetIndex);
-    set({ images: newImages });
   },
 
   title: '',
   description: '',
   setTitle: (title) => set({ title }),
   setDescription: (description) => set({ description }),
+
+  scores: {
+    flavor: 0,
+    sweetness: 0,
+    acidity: 0,
+    balance: 0,
+    overall: 0,
+  },
+  setScore: (key: keyof typeof COFFEE_SCORE_TITLE, value: number) => {
+    set((state) => ({
+      scores: {
+        ...state.scores,
+        [key]: value,
+      },
+    }));
+  },
 }));
