b447a447fc5c404c77e3958b3616fafe19275bd5
Kim Da Eun <midekuna@gmail.com>
2025-02-18 00:25:29 +0900

    chore: CoffeeMetaStep 스타일 수정

--- app/component/CoffeeMetaStep/styles.css.ts
+++ app/component/CoffeeMetaStep/styles.css.ts
@@ -4,6 +4,10 @@ const container = style({
   display: 'flex',
   flexDirection: 'column',
   gap: '24px',
+  justifyContent: 'center',
+  alignItems: 'center',
+
+  margin: '24px 0',
 });
 
 export const styles = {
